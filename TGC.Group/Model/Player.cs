c68a8177bbf5de62106f5ad0e31df1172a01fc7e
CimimUxMaio <cimimuxmaio@gmail.com>
2020-04-25 17:17:49 -0300

    Agrego logs para tratar de arreglar la rotacion del mesh del player

--- TGC.Group/Model/Player.cs
+++ TGC.Group/Model/Player.cs
@@ -20,7 +20,7 @@ namespace TGC.Group.Model
             Mesh = playerScene.Meshes[0];
             Mesh.Scale = new TGCVector3(0.1f, 0.1f, 0.1f);
             Mesh.Position = new TGCVector3(0, 100, -1000);
-            LookDirection = new TGCVector3(0, 0, -1);  // Por como esta orientado el Mesh
+            LookDirection = new TGCVector3(0, 0, -1);  // Por como esta orientado el Mesh originalmente
         }
 
         #region GameObject
@@ -45,7 +45,7 @@ namespace TGC.Group.Model
         #region PRIVATE_METHODS
 
         // <summary>
-        //      Dado un input WASD el Mesh se mueve a la izquierda, derecha, adelante y atras respecto del vector LookDirection
+        //      Dado un input WASD el Mesh se mueve a la izquierda, derecha, adelante y atras respecto del vector LookDirection.
         // <summary>
         private void ManageMovement()
         {
@@ -70,7 +70,7 @@ namespace TGC.Group.Model
                 TGCMatrix rotationMatrix = TGCMatrix.RotationY(FastMath.PI_HALF * 3);
                 movementDirection = ApplyTransformation(rotationMatrix, LookDirection);
             }
-            else if (input.keyDown(Key.Space))
+            else if (input.keyDown(Key.Space))    // De acá para abajo son solo de prueba para poder moverme en la escena libremente
             {
                 movementDirection = TGCVector3.Up;
             }
@@ -78,11 +78,13 @@ namespace TGC.Group.Model
             {
                 movementDirection = -TGCVector3.Up;
             }
-            else if (input.keyDown(Key.Q))
+            else if (input.keyDown(Key.Q))   // No funca
             {
-                float angle = movementSpeed / 50 * GameInstance.ElapsedTime;
-                Mesh.Rotation -= new TGCVector3(0, angle, 0);
+                System.Console.WriteLine("Q pressed");
+                System.Console.WriteLine("Previous rotation: " + Mesh.Rotation);
+                Mesh.Rotation -= new TGCVector3(0, 1000 * GameInstance.ElapsedTime, 0);
                 Mesh.Transform = TGCMatrix.RotationY(Mesh.Rotation.Y);
+                System.Console.WriteLine("Final rotation: " + Mesh.Rotation);
             }
 
             if (!CollisionDetected())
@@ -91,6 +93,9 @@ namespace TGC.Group.Model
             }
         }
 
+        // <summary>
+        //      Multiplico la matriz por el vector a transformar.
+        // <summary>
         private TGCVector3 ApplyTransformation(TGCMatrix rotationMatrix, TGCVector3 vector)
         {
             TGCVector3 result = TGCVector3.Empty;
@@ -101,7 +106,7 @@ namespace TGC.Group.Model
             return result;
         }
 
-        private bool CollisionDetected() => false;  // TODO
+        private bool CollisionDetected() => false;  // TODO Tal vez podria ir en la clase GameObject
 
         #endregion
 
