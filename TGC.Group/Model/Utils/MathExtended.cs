bad1680a3b0d44c9c17b9cce5f019177c549e41d
CimimUxMaio <cimimuxmaio@gmail.com>
2020-04-27 14:49:38 -0300

    Creo clase estatica con funciones de matematica utiles que no estan en FastMath

---
+++ TGC.Group/Model/Utils/MathExtended.cs
@@ -0,0 +1,19 @@
+﻿using TGC.Core.Mathematica;
+
+namespace TGC.Group.Model.Utils
+{
+    public static class MathExtended
+    {
+        public static float NormOfVector3(TGCVector3 v) => FastMath.Sqrt(v.X * v.X + v.Y * v.Y + v.Z * v.Z);
+
+        public static TGCVector3 TransformVector3(TGCMatrix transform, TGCVector3 vector)
+        {
+            TGCVector3 result = TGCVector3.Empty;
+            result.X = transform.M11 * vector.X + transform.M12 * vector.Y + transform.M13 * vector.Z;
+            result.Y = transform.M21 * vector.X + transform.M22 * vector.Y + transform.M23 * vector.Z;
+            result.Z = transform.M31 * vector.X + transform.M32 * vector.Y + transform.M33 * vector.Z;
+
+            return result;
+        }
+    }
+}
